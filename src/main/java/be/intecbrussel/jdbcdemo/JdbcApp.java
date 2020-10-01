23725668698547d4710cf356cf6d0c14d01237c1
BartIntec <bart.hunerbein@intecbrussel.be>
2020-10-01 12:05:47 +0200

    Initial commit

---
+++ src/main/java/be/intecbrussel/jdbcdemo/JdbcApp.java
@@ -0,0 +1,29 @@
+package be.intecbrussel.jdbcdemo;
+
+import java.io.IOException;
+import java.sql.*;
+
+public class JdbcApp {
+
+    public static void main(String[] args) {
+        String connectionString = "jdbc:mysql://localhost:3306/beersdb";
+        String username = "root";
+        String password = "root";
+
+    try (Connection connection = DriverManager.getConnection(connectionString, username, password)) {
+        // ask for a statement
+        Statement statement = connection.createStatement();
+        //use this statement to execute a query
+        ResultSet resultSet = statement.executeQuery("select  * from beers");
+        //print all names from our result of the query
+        while (resultSet.next()){
+            System.out.println(resultSet.getString("Name"));
+        }
+        // do database things
+    } catch (SQLException exception) {
+        exception.printStackTrace();
+    }
+
+    }
+
+}

75c32a57170508ac2abb67f3e4737754eb8ed1fd
michaelarayabe <michaelseifuaraya@gmail.com>
2020-10-22 11:57:50 +0200

    testing

--- src/main/java/be/intecbrussel/jdbcdemo/apps/TestApp.java
+++ src/main/java/be/intecbrussel/jdbcdemo/apps/TestApp.java
@@ -3,5 +3,9 @@ package be.intecbrussel.jdbcdemo.apps;
 public class TestApp {
     public static void main(String[] args) {
         System.out.println("Hello I am the branch!");
+        System.out.println("Another idea");
+        System.out.println("Another idea");
+        System.out.println("Another idea");
+        System.out.println("Another idea");
     }
 }
